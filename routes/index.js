b8d6f6d763e6ffa5a13aa05c8f5086ac3b34a0e8
Torstein Bjørnstad <tosh@tosh.no>
2013-03-17 11:30:57 +0100

    resize icon

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,33 @@
 var color = require('onecolor'),
     Canvas = require('canvas');
 
+function getSolidColorCanvas(size, color){
+  var canvas = new Canvas(size, size),
+    ctx = canvas.getContext('2d');
+
+  ctx.fillStyle = color;
+  ctx.fillRect(0,0,size,size);
+  ctx.fill();
+  return canvas;
+}
+
 exports.index = function(req, res){
   var colorName = req.route.params[0],
     col = color(colorName),
     cHex,
-    canvas = new Canvas(200,200),
-    ctx = canvas.getContext('2d');
+    canvas;
 
   if(!col){
     colorName = 'black';
   }
 
   cHex = color(colorName).hex();
-
-  ctx.fillStyle = cHex;
-  ctx.fillRect(0,0,200,200);
-  ctx.fill();
-
+  canvas = getSolidColorCanvas(64, cHex);
   canvas.toDataURL('image/png', function (err, dataUrl){
     res.render('index', {
       colorName: colorName,
       color: cHex,
-      imageUrl: dataUrl
+      imageUrl: dataUrl,
     });
   });
 };
